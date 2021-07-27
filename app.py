f69d989fddea75da87174b972d36d81a370128b4
Randall <rw15006@usc.edu>
2021-07-27 14:37:11 -0600

    more development

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from datetime import datetime
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import sqlite3
 import csv
-import os 
+import hashlib
+import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///item.db'
 app.secret_key = "random dancing"
@@ -21,6 +22,10 @@ login_manager.init_app(app)
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True)
+    salt = db.Column(db.String(200), unique=False)
+    key = db.Column(db.String(200), unique=False)
+
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -34,6 +39,8 @@ class Item(db.Model):
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     description = db.Column(db.String(200), nullable=False)
     added_by = db.Column(db.String(30), nullable=False)
+    serialNo = db.Column(db.String(50), nullable=False)
+    owner = db.Column(db.String(20), nullable=False)
     # Function to return string when we add something
     def __rep__(self):
         return '<Unique ID %r>' % self.id
@@ -56,28 +63,40 @@ def index():
 def unknown():
     items = Item.query.order_by(Item.date_created)
     masterList = []
+    header = ['ID','Brand','Category','Owner','Date added','Serial no.','Added by']
     for item in items:
         stuff=[]
         stuff.append(item.id)
         stuff.append(item.description)
-        stuff.append(item.date_created)
         stuff.append(item.category)
+        stuff.append(item.owner)
+        stuff.append(item.date_created)
+        stuff.append(item.serialNo)
+        stuff.append(item.added_by)
         masterList.append(stuff)
-    with open('example.csv', mode='w') as employee_file:
+    with open('inventory.csv', mode='w', newline="") as employee_file:
         employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        employee_writer.writerow(header)
         for sublist in masterList:
             employee_writer.writerow(sublist)
-    return send_file("example.csv", as_attachment=True)
+    return send_file("inventory.csv", as_attachment=True)
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
     username = request.form.get("username")
+
     try:
         user = User.query.filter_by(username=username).first()
-        login_user(user)
-        return redirect(url_for('dashboard'))
-    except:
-        print("login failed --------------")
+        new_key = hashlib.pbkdf2_hmac('sha256', request.form.get("password").encode('utf-8'), user.salt, 100000)
+
+        if user.key == new_key:
+            login_user(user)
+            return redirect(url_for('dashboard'))
+        else:
+            session['errorMessage'] = "Invalid credentials."
+            return redirect(url_for('index'))
+        
+    except Exception as e:
         session['errorMessage'] = "Invalid credentials."
         return redirect(url_for('index'))
 
@@ -114,7 +133,6 @@ def view_inventory():
     return render_template("inventory.html")
 
 @app.route("/add_item", methods=['POST','GET'])
-@login_required
 def create_item():
     if request.method == 'GET':
         return render_template("createItem.html")
@@ -122,6 +140,8 @@ def create_item():
         # add item to database
         errorMessage = ""
         description = None
+        serialNo = "N/A"
+        owner = "N/A"
         try:
             description = request.form["description"]
             category = request.form['category']
@@ -131,7 +151,24 @@ def create_item():
         if not description:
             errorMessage="Error: you left one or more fields empty."
             return render_template("error.html", errorMessage=errorMessage)
-        new_item = Item(description=description, category=category, added_by=current_user.username)
+        if category == "Laptop" or category == "Monitor":
+            try:
+                owner = request.form['radioBox']
+                serialNo = request.form['serialNo']
+            except:
+                errorMessage="There was an error processing your request."
+                return render_template("error.html", errorMessage=errorMessage)
+        if category == "Headset":
+            try:
+                owner = request.form['radioBox']
+            except:
+                errorMessage="There was an error processing your request."
+                return render_template("error.html", errorMessage=errorMessage)
+
+        
+
+        new_item = Item(description=description, category=category, added_by=current_user.username, serialNo=serialNo, owner=owner)
+
         try:
             db.session.add(new_item)
             db.session.commit()
